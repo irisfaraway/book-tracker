110d6d619da2962e157958185138fe32fff78f6e
irisfaraway <iris.faraway@gmail.com>
2016-05-22 19:30:03 +0100

    page count validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,11 +7,14 @@ class Book < ActiveRecord::Base
   validates :title, :author, :start_date, presence: true
 
   # Restrict length of titles and authors
-  validates :title, :author, length: { maximum: 50 }
+  validates :title, :author, length: { maximum: 100 }
 
-  # End date must be after or equal to start date, or blank
+  # Number of pages must be an integer above 0, or nil
+  validates :number_of_pages, numericality: { only_integer: true, greater_than_or_equal_to: 1, allow_nil: true }
+
+  # End date must be after or equal to start date, or nil
   validates :end_date, date: { after_or_equal_to: :start_date, allow_nil: true }
 
-  # Rating must be an integer between 1 and 5, or blank
+  # Rating must be an integer between 1 and 5, or nil
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, allow_nil: true }
 end
