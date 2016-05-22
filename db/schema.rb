0d98d06516447d066e09ad8bc27511bde11e910a
irisfaraway <iris.faraway@gmail.com>
2016-05-22 19:21:53 +0100

    generated page number migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317192202) do
+ActiveRecord::Schema.define(version: 20160522182030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(version: 20160317192202) do
     t.date     "start_date"
     t.date     "end_date"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "user_id"
+    t.decimal  "number_of_pages"
   end
 
   add_index "books", ["user_id"], name: "index_books_on_user_id", using: :btree
