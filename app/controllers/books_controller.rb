56b6566f4fdcba1b130e84ae79ef571e9f33a7a6
irisfaraway <iris.faraway@gmail.com>
2016-05-28 17:45:49 +0100

    refactored book stats into separate methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,41 +8,10 @@ class BooksController < ApplicationController
     if logged_in?
       # List the user's books, starting with unfinished ones and then most recently finished
       @books = Book.where(user_id: current_user.id).order('end_date IS NOT NULL, end_date DESC, start_date DESC')
-
-      # Book stats for dashboard
-
-      # Count how many books were finished this year
-      @finished_this_year = @books.where('end_date > ?', Time.zone.today.beginning_of_year).count
-
-      # Get the average time it took to read each book
-      if @finished_this_year.nonzero?
-        # Gets number of current day in entire year, divides it by number of books with end dates this year
-        @days_per_book = Time.zone.today.yday / @finished_this_year
-        # TODO: get actual number of days in year to avoid dodgy leap year stats
-        @books_per_year = 365 / @days_per_book
-      else
-        @days_per_book = 0
-      end
-
-      # Count how many books are still being read
-      @in_progress = @books.where('end_date IS NULL').count
-
-      # Average book ratings
-      if @books.where('rating IS NOT NULL AND end_date >= ?', Time.zone.today.beginning_of_year).empty?
-        @average_this_year = 0
-      else
-        @average_this_year = @books.where('end_date >= ?', Time.zone.today.beginning_of_year).average('rating').round(1)
-      end
-      @average_overall = if @books.where('rating IS NOT NULL').empty?
-                           0
-                         else
-                           @books.average('rating').round(1)
-                         end
-
-      # Page count stats
-      @page_count_this_year = @books.where('end_date >=? AND number_of_pages IS NOT NULL', Date.today.beginning_of_year).sum('number_of_pages')
-      @page_count_overall = @books.where('end_date IS NOT NULL AND number_of_pages IS NOT NULL').sum('number_of_pages')
-
+      count_finished_and_in_progress_books
+      calculate_average_reading_time
+      calculate_average_book_ratings
+      count_pages_read
     else
       flash[:notice] = 'You need to log in first'
       redirect_to(root_path)
@@ -106,13 +75,54 @@ class BooksController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
+  # Use callbacks to share common setup or constraints between actions
   def set_book
     @book = Book.find(params[:id])
     unless @book.user.id == current_user.id
       flash[:warning] = "You can't edit someone else's book"
       redirect_to(books_path)
     end
+  end
+
+  # Calculate book stats for dashboard
+  def count_finished_and_in_progress_books
+    # Count how many books were finished this year
+    @finished_this_year = @books.where('end_date > ?', Time.zone.today.beginning_of_year).count
+
+    # Count how many books are still being read
+    @in_progress = @books.where('end_date IS NULL').count
+  end
+
+  # Average time it took to read each book this year
+  def calculate_average_reading_time
+    if @finished_this_year.nonzero?
+      # Gets number of current day in entire year, divides it by number of books with end dates this year
+      @days_per_book = Time.zone.today.yday / @finished_this_year
+      # TODO: get actual number of days in year to avoid dodgy leap year stats
+      @books_per_year = 365 / @days_per_book
+    else
+      @days_per_book = 0
+    end
+  end
+
+  # Average book ratings this year and overall
+  def calculate_average_book_ratings
+    @average_this_year = if @books.where('rating IS NOT NULL AND end_date >= ?', Time.zone.today.beginning_of_year).empty?
+                           0
+                         else
+                           @books.where('end_date >= ?', Time.zone.today.beginning_of_year).average('rating').round(1)
+                         end
+    @average_overall = if @books.where('rating IS NOT NULL').empty?
+                         0
+                       else
+                         @books.average('rating').round(1)
+                       end
+  end
+
+  # Count number of pages read this year and overall
+  def count_pages_read
+    @page_count_this_year = @books.where('end_date >=? AND number_of_pages IS NOT NULL', Date.today.beginning_of_year).sum('number_of_pages')
+    @page_count_overall = @books.where('end_date IS NOT NULL AND number_of_pages IS NOT NULL').sum('number_of_pages')
   end
 
   # Search Google Books and return results
