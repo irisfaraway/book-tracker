af68065117d143033523f79d176a8bbdd7f2fd7b
irisfaraway <iris.faraway@gmail.com>
2016-05-28 17:32:40 +0100

    fixing unless-else, appeasing rubocop

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,16 +8,16 @@ class BooksController < ApplicationController
     if logged_in?
       # List the user's books, starting with unfinished ones and then most recently finished
       @books = Book.where(user_id: current_user.id).order('end_date IS NOT NULL, end_date DESC, start_date DESC')
-      
+
       # Book stats for dashboard
 
       # Count how many books were finished this year
-      @finished_this_year = @books.where('end_date > ?', Date.today.beginning_of_year).count
+      @finished_this_year = @books.where('end_date > ?', Time.zone.today.beginning_of_year).count
 
       # Get the average time it took to read each book
       if @finished_this_year.nonzero?
         # Gets number of current day in entire year, divides it by number of books with end dates this year
-        @days_per_book = Date.today.yday / @finished_this_year
+        @days_per_book = Time.zone.today.yday / @finished_this_year
         # TODO: get actual number of days in year to avoid dodgy leap year stats
         @books_per_year = 365 / @days_per_book
       else
@@ -28,23 +28,23 @@ class BooksController < ApplicationController
       @in_progress = @books.where('end_date IS NULL').count
 
       # Average book ratings
-      unless @books.where('rating IS NOT NULL AND end_date >= ?', Date.today.beginning_of_year).empty?
-        @average_this_year = @books.where('end_date >= ?', Date.today.beginning_of_year).average('rating').round(1)
-      else
+      if @books.where('rating IS NOT NULL AND end_date >= ?', Time.zone.today.beginning_of_year).empty?
         @average_this_year = 0
-      end
-      unless @books.where('rating IS NOT NULL').empty?
-        @average_overall = @books.average('rating').round(1)
       else
-        @average_overall = 0
+        @average_this_year = @books.where('end_date >= ?', Time.zone.today.beginning_of_year).average('rating').round(1)
       end
+      @average_overall = if @books.where('rating IS NOT NULL').empty?
+                           0
+                         else
+                           @books.average('rating').round(1)
+                         end
 
       # Page count stats
       @page_count_this_year = @books.where('end_date >=? AND number_of_pages IS NOT NULL', Date.today.beginning_of_year).sum('number_of_pages')
       @page_count_overall = @books.where('end_date IS NOT NULL AND number_of_pages IS NOT NULL').sum('number_of_pages')
 
     else
-      flash[:notice] = "You need to log in first"
+      flash[:notice] = 'You need to log in first'
       redirect_to(root_path)
     end
   end
@@ -105,27 +105,28 @@ class BooksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-      unless @book.user.id == current_user.id
-        flash[:warning] = "You can't edit someone else's book"
-        redirect_to(books_path)
-      end
-    end
 
-    # Search Google Books and return results
-    def search_google_books
-      @google_books_results = GoogleBooks.search(params[:google_books_query], {:country => "uk"})
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find(params[:id])
+    unless @book.user.id == current_user.id
+      flash[:warning] = "You can't edit someone else's book"
+      redirect_to(books_path)
     end
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def book_params
-      params.require(:book).permit(:title,
-                                    :author,
-                                    :number_of_pages,
-                                    :start_date,
-                                    :end_date,
-                                    :rating)
-    end
+  # Search Google Books and return results
+  def search_google_books
+    @google_books_results = GoogleBooks.search(params[:google_books_query], { :country => 'uk' })
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def book_params
+    params.require(:book).permit(:title,
+                                 :author,
+                                 :number_of_pages,
+                                 :start_date,
+                                 :end_date,
+                                 :rating)
+  end
 end
