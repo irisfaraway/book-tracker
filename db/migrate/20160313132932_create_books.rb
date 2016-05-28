18157bed6a0b922d17ecdde2176849d7eae095fe
irisfaraway <iris.faraway@gmail.com>
2016-05-28 16:32:09 +0100

    fixing dates in book migration

--- db/migrate/20160313132932_create_books.rb
+++ db/migrate/20160313132932_create_books.rb
@@ -3,8 +3,8 @@ class CreateBooks < ActiveRecord::Migration
     create_table :books do |t|
       t.string :title
       t.string :author
-      t.date :start
-      t.date :end
+      t.date :start_date
+      t.date :end_date
       t.integer :rating
 
       t.timestamps null: false
