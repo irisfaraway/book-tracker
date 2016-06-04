4475aeb66afbb823634e63246fd1d5e43385a403
irisfaraway <iris.faraway@gmail.com>
2016-06-04 17:05:18 +0100

    adjusted permissions, user tests now work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   # Must be logged in to do anything user-related
   before_action :logged_in_user
-  # Only admins can edit and destroy users
-  before_action :admin_user, only: [:edit, :update, :destroy]
+  # Must be an admin to do anything user-related
+  before_action :admin_user
 
   # GET /users
   # GET /users.json
@@ -72,7 +73,6 @@ class UsersController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
-    redirect_to(root_url) unless @user == current_user
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,9 +3,15 @@ require 'test_helper'
 # Tests for users
 class UsersControllerTest < ActionController::TestCase
   setup do
+    # Get user fixtures
     @admin_user = users(:admin_user)
-    @normal_user = users(:normal_user)
-    @delete_user = users(:delete_user)
+    @create_user = users(:create_user)
+    @show_user = users(:show_user)
+    @edit_user = users(:edit_user)
+    @update_user = users(:update_user)
+    @destroy_user = users(:destroy_user)
+
+    # Set current user
     session[:user_id] = @admin_user.id
   end
 
@@ -22,30 +28,35 @@ class UsersControllerTest < ActionController::TestCase
 
   test 'should create user' do
     assert_difference('User.count') do
-      post :create, id: @admin_user, user: { name: 'Julie Andrews' }
+      post :create, id: @create_user, user: { name: 'Julie Andrews' }
     end
 
     assert_redirected_to user_path(assigns(:user))
   end
 
   test 'should show user' do
-    get :show, id: @admin_user
+    get :show, id: @update_user
     assert_response :success
   end
 
   test 'should get edit' do
-    get :edit, id: @admin_user
+    get :edit, id: @edit_user
     assert_response :success
   end
 
   test 'should update user' do
-    patch :update, id: @admin_user, user: { name: 'Drimble Wedge' }
+    patch :update, id: @update_user, user: { provider: @show_user.provider,
+                                             uid: @show_user.uid,
+                                             name: @show_user.name,
+                                             token: @show_user.token,
+                                             expires_at: @show_user.expires_at,
+                                             admin: @show_user.admin }
     assert_redirected_to user_path(assigns(:user))
   end
 
   test 'should destroy user' do
     assert_difference('User.count', -1) do
-      delete :destroy, id: @delete_user
+      delete :destroy, id: @destroy_user
     end
 
     assert_redirected_to users_path
